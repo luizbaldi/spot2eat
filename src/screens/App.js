3895832fe6255a3c156c2f22f8ae3ce96b166e38
Luiz Baldi <baldilp@gmail.com>
2017-07-08 13:29:53 -0300

    adding axios to project and login validation with myJson api

--- src/screens/App.js
+++ src/screens/App.js
@@ -3,12 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './login/Login';
 import Dashboard from './dashboard/Dashboard';
 
+
 class App extends Component {
     render() {
         return (
             <Switch>
-            	<Route exact path='/' component={ Login } />
-            	<Route path='/dashboard' component={ Dashboard } />
+            	<Route exact path='/' component={Login} />
+            	<Route path='/dashboard' component={Dashboard} />
             </Switch>
         );
     }
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,41 +1,75 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 
 class Login extends Component {
 	constructor(props) {
 		super(props);
 
+		this.state = {
+			email: '',
+			password: ''
+		};
+
 		this.validateLogin = this.validateLogin.bind(this);
+		this.onEmailChange = this.onEmailChange.bind(this);
+		this.onPasswordChange = this.onPasswordChange.bind(this);
 	}
 	validateLogin() {
-		// Add login validation later on
-		let isLoginValid = true;
-
-		if (isLoginValid) {
-			this.props.history.push('/dashboard');
+		let email = this.state.email;
+		let password = this.state.password;
+		if (email && password) {
+	  		axios.get("https://api.myjson.com/bins/8e5l3")
+				.then(response => {
+					let currentUser;
+					let isLoginValid = response.data.some(user => {
+						if (email === user.email && password === user.password) {
+							currentUser = user;
+							return true;
+						}
+					})
+					if (isLoginValid) {
+						alert(`Welcome ${currentUser.name} :)`);
+						this.props.history.push('/dashboard');
+					} else {
+						alert("Invalid login.");
+					}
+				})
+				.catch(err => {
+					alert("Error during loggin in :(");
+				});
 		} else {
-			console.log("Login not valid.");
+			alert("Please type your e-mail and password to procced :)");
 		}
+	}
+	onEmailChange(e) {
+		this.setState({
+			email: e.target.value
+		});
+	}
+	onPasswordChange(e) {
+		this.setState({
+			password: e.target.value
+		});
 	}
 	render() {
 		return (
 			<div className="app-container bg-color-default">
-				<div className="panel vertically-center">
+				<div className="panel vertically-center text-center">
 					<h4>spot2eat</h4>
 					<form>
 						E-mail:
 						<br />
-						<input type="text" name="email" />
+						<input type="text" name="email" onChange={this.onEmailChange} />
 						<br />
 						Senha:
 						<br />
-						<input type="text" name="password" />
+						<input type="password" name="password" onChange={this.onPasswordChange} />
 						<br />
-						<button onClick={this.validateLogin}>Login</button>
+						<button onClick={this.validateLogin} type="button">Login</button>
 						<hr />
 						<Link to="/signup">
-							<button>Cadastre-se</button>
+							<button type="button">Cadastre-se</button>
 						</Link>
 					</form>
 				</div>
